828565b517c64abddcd7e9c8cfb8adcc342cd7a3
Cory <Coryt0121@gmail.com>
2017-10-06 17:34:43 -0400

    Scrollable timeline

--- front-end/src/components/timeline/Timeline.jsx
+++ front-end/src/components/timeline/Timeline.jsx
@@ -14,14 +14,8 @@ class Timeline extends Component {
     setSelectedTimeline: PropTypes.func.isRequired,
     entireTimelineData: PropTypes.arrayOf(
       PropTypes.shape({
-        congenital_anomalies: PropTypes.number.isRequired,
-        deaths: PropTypes.number.isRequired,
-        disablilities: PropTypes.number.isRequired,
-        hospitalizations: PropTypes.number.isRequired,
-        init_fda: PropTypes.number.isRequired,
-        life_threatenings: PropTypes.number.isRequired,
-        other_serious: PropTypes.number.isRequired,
-        required_interventions: PropTypes.number.isRequired,
+        serious: PropTypes.number.isRequired,
+        not_serious: PropTypes.number.isRequired,
       }),
     ).isRequired,
   }
@@ -70,50 +64,6 @@ class Timeline extends Component {
         }
       }
     }, true);
-    
-    // document.getElementById('timeline-chart').addEventListener('wheel', (e) => {
-    //   // console.log(e)
-    //   if (e.deltaY > 0) {
-    //     this.setState({ brushStartIndex: this.state.brushStartIndex + 1 });
-    //     this.setState({ brushStartIndex: this.state.brushEndIndex + 1 });
-    //   } else if (e.deltaY < 0) {
-    //     if (this.brushStartIndex > 0) {
-    //       this.setState({ brushStartIndex: this.state.brushStartIndex - 1 });
-    //       this.setState({ brushStartIndex: this.state.brushEndIndex - 1 });
-    //     }
-    //   } else {
-    //     console.log('Delta is 0');
-    //   }
-    //   e.preventDefault();
-    // }, true);
-  }
-
-  // handleZoom(domain) {
-  //   this.setState({selectedDomain: domain});
-  // }
-
-  // handleBrush(domain) {
-  //   this.setState({zoomDomain: domain});
-  // }
-
-  clickAreaChart = (e) => {
-    console.log(e);
-    // if (e && e.activeLabel) {
-    //   if (this.state.setStart) {
-    //     this.setState({ selectedStartX: (e.activeLabel), setStart: false });
-    //     this.setState({ previewStartX: (e.activeLabel), setStart: false });
-    //   } else {
-    //     this.setState({ selectedEndX: (e.activeLabel), setStart: true });
-    //     this.setState({ previewdEndX: (e.activeLabel), setStart: true });
-    //   }
-    //   console.log(this.state.selectedStartX);
-    //   console.log(this.state.selectedEndX);
-    // }
-  }
-
-  brushChange = (a, b, c, d) => {
-    // super(a, b, c, d);
-    console.log(a, b, c, d)
   }
 
   updateSelectedDate = () => {
@@ -134,38 +84,6 @@ class Timeline extends Component {
   }
 
   data = () => this.props.entireTimelineData;
-  // data = [
-  //   { d: 150, date: 20170401 },
-  //   { d: 200, date: 20170402 },
-  //   { d: 175, date: 20170403 },
-  //   { d: 225, date: 20170404 },
-  //   { d: 230, date: 20170405 },
-  //   { d: 235, date: 20170406 },
-  //   { d: 150, date: 20170407 },
-  //   { d: 200, date: 20170408 },
-  //   { d: 175, date: 20170409 },
-  //   { d: 225, date: 20170410 },
-  //   { d: 230, date: 20170411 },
-  //   { d: 235, date: 20170412 },
-  //   { d: 150, date: 20170413 },
-  //   { d: 200, date: 20170414 },
-  //   { d: 175, date: 20170415 },
-  //   { d: 225, date: 20170416 },
-  //   { d: 400, date: 20170417 },
-  //   { d: 735, date: 20170418 },
-  //   { d: 450, date: 20170419 },
-  //   { d: 200, date: 20170420 },
-  //   { d: 175, date: 20170421 },
-  //   { d: 225, date: 20170422 },
-  //   { d: 230, date: 20170423 },
-  //   { d: 235, date: 20170424 },
-  //   { d: 150, date: 20170425 },
-  //   { d: 200, date: 20170426 },
-  //   { d: 175, date: 20170427 },
-  //   { d: 225, date: 20170428 },
-  //   { d: 230, date: 20170429 },
-  //   { d: 235, date: 20170430 },
-  // ];
 
   render = () => (
     <div id="timeline-selector">
@@ -185,18 +103,16 @@ class Timeline extends Component {
           <AreaChartImpl
             data={this.data()}
             margin={{ top: 5, right: 5, left: 5, bottom: 5 }}
-            onClick={this.clickAreaChart}
             onMouseMove={this.recordMouseMove}
-            onScroll={this.scrollHandler}
           >
             <defs>
-              <linearGradient id="colorGreen" x1="0" y1="0" x2="0" y2="1">
+              {/* <linearGradient id="colorGreen" x1="0" y1="0" x2="0" y2="1">
                 <stop offset="15%" stopColor="#82ca9d" stopOpacity={0.8} />
                 <stop offset="99%" stopColor="#82ca9d" stopOpacity={0.2} />
-              </linearGradient>
-              <linearGradient id="colorYellow" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="15%" stopColor="#FFEE58" stopOpacity={0.8} />
-                <stop offset="99%" stopColor="#FFEE58" stopOpacity={0.2} />
+              </linearGradient> */}
+              <linearGradient id="colorGrey" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="15%" stopColor="#757575" stopOpacity={0.8} />
+                <stop offset="99%" stopColor="#757575" stopOpacity={0.2} />
               </linearGradient>
               <linearGradient id="colorRed" x1="0" y1="0" x2="0" y2="1">
                 <stop offset="15%" stopColor="#E53935" stopOpacity={0.8} />
@@ -208,60 +124,8 @@ class Timeline extends Component {
             <Tooltip />
             <Area
               type="monotone"
-              dataKey="hospitalizations"
+              dataKey="serious"
               stroke="#8884d8"
-              fillOpacity={1}
-              stackId="1"
-              fill="url(#colorGreen)"
-              animationDuration={700}
-              connectNulls={false}
-            />
-            <Area
-              type="monotone"
-              dataKey="life_threatenings"
-              stroke="#FDD835"
-              fillOpacity={1}
-              stackId="1"
-              fill="url(#colorYellow)"
-              animationDuration={700}
-              connectNulls={false}
-            />
-            <Area
-              type="monotone"
-              dataKey="deaths"
-              stroke="#E53935"
-              fillOpacity={1}
-              stackId="1"
-              fill="url(#colorRed)"
-              animationDuration={700}
-              connectNulls={false}
-            />
-
-            {/* COPPIED HERE */}
-            <Area
-              type="monotone"
-              dataKey="required_interventions"
-              stroke="#8884d8"
-              fillOpacity={1}
-              stackId="1"
-              fill="url(#colorGreen)"
-              animationDuration={700}
-              connectNulls={false}
-            />
-            <Area
-              type="monotone"
-              dataKey="other_serious"
-              stroke="#FDD835"
-              fillOpacity={1}
-              stackId="1"
-              fill="url(#colorYellow)"
-              animationDuration={700}
-              connectNulls={false}
-            />
-            <Area
-              type="monotone"
-              dataKey="disablilities"
-              stroke="#E53935"
               fillOpacity={1}
               stackId="1"
               fill="url(#colorRed)"
@@ -270,16 +134,14 @@ class Timeline extends Component {
             />
             <Area
               type="monotone"
-              dataKey="congenital_anomalies"
-              stroke="#8884d8"
+              dataKey="not_serious"
+              stroke="#424242"
               fillOpacity={1}
               stackId="1"
-              fill="url(#colorGreen)"
+              fill="url(#colorGrey)"
               animationDuration={700}
               connectNulls={false}
             />
-
-
             <BrushImpl
               dataKey="init_fda_dt"
               stroke="#8884d8"
